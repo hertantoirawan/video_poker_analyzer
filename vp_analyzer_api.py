48e34b3d67dd96f168346551f0df4b483a3a7b26
Hertanto Irawan <hertanto.irawan@outlook.com>
2021-12-29 16:09:34 +0700

    Test api in heroku

--- vp_analyzer_api.py
+++ vp_analyzer_api.py
@@ -1,17 +1,15 @@
 from flask import Flask
-from vp_analyzer import HandAnalyzer, DiscardValue
+from vp_analyzer import HandAnalyzer
 
 app = Flask(__name__)
 
 @app.route("/analyze/<hand>")
 def analyze_api(hand):
     response = HandAnalyzer(hand).analyze(return_full_analysis=False, return_bestdisc_cnts = True)
-    response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
 @app.errorhandler(404)
 def page_not_found(error):
     response = "<p>Use /analyze/&lthand&gt </p><p>Hands are represented as 10-character long strings, with a card rank character followed by a suit character. The expected rank characters are: A, 2, 3, 4, 5, 6, 7, 8, 9, T, J, Q, K. The expected suit characters are: c, d, h, s. When dealing with discards, cards to be replaced are represented by 'XX'. For example, a hand containing: Three of Clubs, Ace of Hearts, Three of Diamonds, Ten of Hearts, Jack of Spades; is '3cAh3dThJs' and one discard strategy would be to hold the pair of threes: '3cXX3dXXXX'.</p>"
-    response.headers.add('Access-Control-Allow-Origin', '*')
     return response    
     
