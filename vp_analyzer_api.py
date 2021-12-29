3407f210219de3a43bf15cc0faeca3227c400f68
Hertanto Irawan <hertanto.irawan@outlook.com>
2021-12-29 16:46:16 +0700

    Add flask_cors

--- vp_analyzer_api.py
+++ vp_analyzer_api.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 from vp_analyzer import HandAnalyzer
 
-app = Flask(__name__)
+app = Flask(__name__) 
+CORS(app)
 
 @app.route("/analyze/<hand>")
 def analyze_api(hand):
