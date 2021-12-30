13d815edc8c4b40d512a667724166f7be2c8e8f1
Hertanto Irawan <hertanto.irawan@outlook.com>
2021-12-30 12:23:19 +0700

    Change error response into json

--- vp_analyzer_api.py
+++ vp_analyzer_api.py
@@ -5,13 +5,16 @@ from vp_analyzer import HandAnalyzer
 app = Flask(__name__) 
 CORS(app)
 
-@app.route("/analyze/<hand>")
+@app.route("/analyze/<hand>", methods=['GET'])
 def analyze_api(hand):
-    response = HandAnalyzer(hand).analyze(return_full_analysis=False, return_bestdisc_cnts = True)
-    return response
+    return HandAnalyzer(hand).analyze(return_full_analysis=False, return_bestdisc_cnts = True)
 
 @app.errorhandler(404)
 def page_not_found(error):
-    response = "<p>Use /analyze/&lthand&gt </p><p>Hands are represented as 10-character long strings, with a card rank character followed by a suit character. The expected rank characters are: A, 2, 3, 4, 5, 6, 7, 8, 9, T, J, Q, K. The expected suit characters are: c, d, h, s. When dealing with discards, cards to be replaced are represented by 'XX'. For example, a hand containing: Three of Clubs, Ace of Hearts, Three of Diamonds, Ten of Hearts, Jack of Spades; is '3cAh3dThJs' and one discard strategy would be to hold the pair of threes: '3cXX3dXXXX'.</p>"
-    return response    
+    response = "Use /analyze/&lthand&gt </p><p>Hands are represented as 10-character long strings, with a card rank character followed by a suit character. The expected rank characters are: A, 2, 3, 4, 5, 6, 7, 8, 9, T, J, Q, K. The expected suit characters are: c, d, h, s. When dealing with discards, cards to be replaced are represented by 'XX'. For example, a hand containing: Three of Clubs, Ace of Hearts, Three of Diamonds, Ten of Hearts, Jack of Spades; is '3cAh3dThJs' and one discard strategy would be to hold the pair of threes: '3cXX3dXXXX'."
+    
+    return {
+        "error": 404,
+        "message": response
+    }    
     
